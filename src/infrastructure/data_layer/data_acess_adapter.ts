7a46e34d8af5921b17ba1b10d18dfd2fe0f77aba
Bruna Ribeiro <bruna.ribeiro@brf.com>
2022-11-17 11:14:29 -0300

    change mongo repository

--- src/infrastructure/data_layer/data_acess_adapter.ts
+++ src/infrastructure/data_layer/data_acess_adapter.ts
@@ -2,24 +2,28 @@ import Promotions from "../../resources/models/Promotions"
 
 export const getAllPromotionsMongo = async () => {
   const collectionPromotions = await Promotions.find()
-  return collectionPromotions
+  return { collectionPromotions }
 }
 
 export const getPromotionByIdMongo = async (id: any) => {
   const promotionById = await Promotions.findById(id)
-  return promotionById
+  return { promotionById }
 }
 
 export const createPromotionMongo = async (data: any) => {
+  const { id_region } = data
+  const promotionExists = await Promotions.findOne({ id_region })
   const createPromotion = await Promotions.create(data)
-  return createPromotion
+
+  return { createPromotion, promotionExists }
 }
 
 export const updatePromotionMongo = async (id: any, data: any) => {
   const promotionUpdate = await Promotions.findByIdAndUpdate(id, data)
-  return promotionUpdate
+  const promotionIdValidation = promotionUpdate?._id
+  return { promotionUpdate, promotionIdValidation }
 }
 export const deletePromotionMongo = async (id: any) => {
-  const promotionDelete = await Promotions.findOneAndDelete(id)
-  return promotionDelete
+  const promotionDelete = await Promotions.findByIdAndDelete(id)
+  return { promotionDelete }
 }
