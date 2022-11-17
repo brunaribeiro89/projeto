25845db6fa184155337fa44c16e950aac1af3737
Bruna Ribeiro <bruna.ribeiro@brf.com>
2022-11-17 11:16:20 -0300

    modified promotion services

--- src/domain/business_rules/service/promotionsServices.ts
+++ src/domain/business_rules/service/promotionsServices.ts
@@ -7,25 +7,38 @@ import {
 } from "../../../infrastructure/data_layer/data_acess_adapter"
 
 export const listPromotions = async () => {
-  const promotionList = await getAllPromotionsMongo()
-  return promotionList
+  const { collectionPromotions } = await getAllPromotionsMongo()
+  if (collectionPromotions.length === 0) {
+    throw Error("There are no registered promotions")
+  }
+  return collectionPromotions
 }
 
 export const getPromotionById = async (id: any) => {
-  const promotionById = await getPromotionByIdMongo(id)
+  const { promotionById } = await getPromotionByIdMongo(id)
   return promotionById
 }
 
 export const createPromotion = async (data: any) => {
-  const promotionCreate = await createPromotionMongo(data)
-  return promotionCreate
+  const { createPromotion, promotionExists } = await createPromotionMongo(data)
+  if (promotionExists) {
+    throw Error("There are no registered promotions")
+  }
+
+  return createPromotion
 }
 
 export const updatePromotion = async (id: any, data: any) => {
-  const promotionUpdate = await updatePromotionMongo(id, data)
+  const { promotionUpdate, promotionIdValidation } = await updatePromotionMongo(id, data)
+  if (!promotionIdValidation) {
+    throw Error("There are no registered promotions")
+  }
   return promotionUpdate
 }
 export const deletePromotion = async (id: any) => {
-  const promotionDelete = await deletePromotionMongo(id)
+  const { promotionDelete } = await deletePromotionMongo(id)
+  if (!promotionDelete) {
+    throw Error("There are no registered promotions")
+  }
   return promotionDelete
 }
